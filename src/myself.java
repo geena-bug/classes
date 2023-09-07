b72345b9dfc47cbc66c061102d133f45ec86fc01
geena_bug <quingina0@gmail.com>
2023-09-07 12:30:01 +0100

    Learning how to use next() and nextLine();

---
+++ src/myself.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class myself {
+
+    public static void main(String[]args){
+        //Declaration section
+String myName;
+        //Input section
+        //Set up scanner to capture input
+        Scanner inputLine = new Scanner(System.in);
+
+        //prompt user to write her name
+        System.out.println("Please write your name:");
+        inputLine.next();
+        //prompt user to write about herself
+        System.out.println("Tell me about yourself:");
+        inputLine.nextLine();
+
+
+    }
+}
